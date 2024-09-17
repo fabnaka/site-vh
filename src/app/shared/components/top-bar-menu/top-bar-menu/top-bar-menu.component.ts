460bc6eee7d32134b12f87854bc42f14c5e92caf
Fábio Shiniti Nakazato <fabio.nakazato@sfiems.com.br>
2024-09-17 16:51:40 -0400

    'assets'

---
+++ src/app/shared/components/top-bar-menu/top-bar-menu/top-bar-menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-top-bar-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './top-bar-menu.component.html',
+  styleUrl: './top-bar-menu.component.scss'
+})
+export class TopBarMenuComponent {
+
+}
